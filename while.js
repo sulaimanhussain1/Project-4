0082ce142b5dc22d1d1df5f2af28174205910937
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-01-05 01:47:05 +0500

    This is the practice file of javascript

---
+++ while.js
@@ -0,0 +1,24 @@
+
+//  while loop 
+
+
+document.write("<h1>WHILE LOOP </h1>")
+var i = 0;
+while( i < 5 )
+  {
+      document.write("SULAIMAN HUSSAIN");
+      document.write(i);
+       i++;
+     document.write("<br>");
+  }
+
+  // main reason to use while loop 
+
+//   var j = 0 ;
+//   while ( true ) 
+//   {
+//       if ( j == 4) 
+//           break;
+//         document.write(j);
+//         document.write("<br>");
+//  } 
