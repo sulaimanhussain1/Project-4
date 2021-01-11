3718cd47fe4158e71bb05a65116785ebd654210a
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-01-11 07:09:37 +0500

    this is the practice file of javascript.

---
+++ typeof.js
@@ -0,0 +1,28 @@
+
+
+// typeof operator 
+
+var a = 19 ;
+var b = "HELLO";
+var c = "L-420";
+var d = (true);
+document.write(typeof(a) + " <br><br>");
+document.write(typeof(b) + " <br><br>");
+document.write(typeof(c) + " <br><br>");
+document.write(typeof(d) + " <br><br>");
+
+// UNDEFINED 
+document.write("<h2>UNDEFINED .</h2>")
+
+var e;
+document.write(e + " <br>");
+// document.write(g);
+
+
+// NULL 
+
+document.write("<h2>NULL.</h2>");
+
+var f = null ;
+document.write( f + " <br>");
+document.write(typeof (f));
