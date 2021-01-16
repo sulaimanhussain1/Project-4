d8ed6e7e262561049df0f5c87f7a3003665b951f
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-01-17 02:43:34 +0500

    This is the practice file of javascript.

---
+++ prototype1.js
@@ -0,0 +1,29 @@
+
+
+// Prototype 
+
+// console.log(Object.prototype);
+// var b = {} ;
+// console.log(Object.getPrototypeOf(b));
+// console.log(Object.getPrototypeOf(Object.prototype));
+
+// Constructor Prototype 
+
+// var a = new Object();
+// console.log(Object.getPrototypeOf(a));
+// console.log(Object.getPrototypeOf(Object.prototype));
+
+// Array.prototype
+
+// var c = new Array();
+// console.log(Object.getPrototypeOf(c));
+// console.log(Object.getPrototypeOf(Array.prototype));
+// console.log(Object.getPrototypeOf(Object.prototype));
+
+// String.prototype
+
+
+var d = new String();
+console.log(Object.getPrototypeOf(d));
+console.log(Object.getPrototypeOf(String.prototype));
+console.log(Object.getPrototypeOf(Object.prototype));
