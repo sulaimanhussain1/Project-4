bb443e394bde0e2f73c2951bbe232ffc8d1811e4
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-01-22 06:36:42 +0500

    This is the practice file of javascript.

---
+++ reverse.js
@@ -0,0 +1,11 @@
+
+
+// Reverse Method ..
+
+document.write("<h1>REVERSE METHOD..</h1>");
+
+
+var funny = ["Sulaiman" , "Shahroz " , "Maaz" , "Saim" ];
+document.write(funny + "<br>") ;
+funny.reverse();
+document.write(funny + "<br>");
