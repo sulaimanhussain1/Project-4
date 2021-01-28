ef74544e5bf8e9a5d45f58a6ee907498c29c5edc
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-01-28 22:22:34 +0500

    This is the practice file of javascript.

---
+++ relational.js
@@ -0,0 +1,72 @@
+
+
+document.write("<h1>RELATIONAL OPERATIONS.</h1><br>");
+document.write("c = 10 > 23 <br>");
+var a = 10;
+var b = 23;
+c = a > b ;
+document.write(c);
+document.write("<br><br>");
+
+
+// document.write("<h1>ARITHMETIC OPERATIONS.</h1><br>");
+document.write("c = 10 < 23 <br>");
+var a = 10;
+var b = 23;
+c = a < b ;
+document.write(c);
+document.write("<br><br>");
+
+
+// document.write("<h1>ARITHMETIC OPERATIONS.</h1><br>");
+document.write("c = 10 == 10 <br>");
+var a = 10;
+var b = 10;
+c = a == b ;
+document.write(c);
+document.write("<br><br>");
+
+
+// document.write("<h1>ARITHMETIC OPERATIONS.</h1><br>");
+document.write("c = 10 <= 23 <br>");
+var a = 10;
+var b = 23;
+c = a <= b ;
+document.write(c);
+document.write("<br><br>");
+
+
+// document.write("<h1>ARITHMETIC OPERATIONS.</h1><br>");
+document.write("c = 10 >= 23 <br>");
+var a = 10;
+var b = 23;
+c = a >= b ;
+document.write(c);
+document.write("<br><br>");
+
+
+// document.write("<h1>ARITHMETIC OPERATIONS.</h1><br>");
+document.write("c = 10 != 23 <br>");
+var a = 10;
+var b = 23;
+c = a != b ;
+document.write(c);
+document.write("<br><br>");
+
+
+// document.write("<h1>ARITHMETIC OPERATIONS.</h1><br>");
+document.write("c = 10  === 10 <br>");
+var a = 10;
+var b = 10;
+c = a === b ;
+document.write(c);
+document.write("<br><br>");
+
+
+// document.write("<h1>ARITHMETIC OPERATIONS.</h1><br>");
+document.write("c = 10 !== 10 <br>");
+var a = 10;
+var b = 10;
+c = a !== b ;
+document.write(c);
+document.write("<br><br>");
