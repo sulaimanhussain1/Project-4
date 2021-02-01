ef5e79e175f9f15f91207e6656ed1364a58e51db
sulaimanhussain1 <hussainsulaiman460@gmail.com>
2021-02-01 20:32:28 +0500

    This is the practice of javascript.

---
+++ return.js
@@ -0,0 +1,16 @@
+
+// RETURN STATEMENT 
+
+ 
+function sum( a , b){
+
+    document.write("JAVASCRIPT <br><br>");
+    return ( a + b );
+
+}
+ var i = sum (39, 40);
+ document.write(i);
+
+
+//  i = sum (39, 40);
+//  document.write(i);
